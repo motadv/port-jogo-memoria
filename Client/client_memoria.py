587851a152f55f5f48665eea73c3fe4ccbe7dc53
motadv <102678270+motadv@users.noreply.github.com>
2023-04-21 21:56:06 -0300

    Implementação Base

--- Client/client_memoria.py
+++ Client/client_memoria.py
@@ -1,19 +1,84 @@
 import socket
 
+
+#! SETTAR IP DO HOST <<<<<
 HOST = "26.44.38.67"
 PORT = 50000
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
 
-REQUEST_INPUT = "REQUEST_INPUT"
+# * Message Settings
+# * Constantes utilizadas tanto por Cliente quanto por Servidor
+# * Definem o tipo de mensagem que irá receber, seja ele um input ou tipo de dado do jogo
+
+
+HEADER_LENGTH = 10
+REQUEST_USERNAME = "#REQUEST_USERNAME_INPUT"
+REQUEST_GAME_INPUT = "#REQUEST_GAME_INPUT"
+SEND_STATUS = "#STATUS"
+SEND_TABULEIRO = "#TABULEIRO"
+SEND_PLACAR = "#PLACAR"
+SEND_VEZ = "#VEZ"
+
+
+# ? Função que garante que você está recebendo uma mensagem do tamanho correto
+# ? para acessar o dado da mensagem, lembre de garantir que ela chegou
+# ? E acesse por receiveMessage(...)['data']
+# ? Caso não precise do header da mensagem na implementação
+# ? implemente diretamente no retorno da função o acesso ao ['data']
+
+
+def receiveMessage(socket):
+    try:
+        message_header = socket.recv(HEADER_LENGTH)
+
+        if not len(message_header):
+            return False
+
+        message_length = int(message_header.decode())
+        return {
+            "header": message_header,
+            "data": socket.recv(message_length).decode()
+        }
+    except:
+        return False
+
+
+def createMessage(message: str):
+    data = message.encode()
+    header = f'{len(data):<{HEADER_LENGTH}}'.encode()
+    return header + data
+
 
 while True:
-    msg = s.recv(1024).decode()  # ? Definir tamanho da mensagem
+    # Exemplo de Utilização do Código (Incompleto):
+    # ? Se quiser modificar o estilo de implementação, avisa pra gente! Vai mudar alguma lógica com certeza
+    # ? Servidor atualmente é incrívelmente dependente em receber exatamente o esperado por parte do client
 
-    if msg == REQUEST_INPUT:
-        sendMessage = input("Enviar para server: ")
-        s.send(sendMessage.encode())
-        continue
+    # Cliente constantemente lê mensagens do servidor
+    msg = receiveMessage(client_socket)
 
-    print(msg)
+    # Verifica se a mensagem foi recebida corretamente (False ou {header, data})
+    if msg:
+        # Acessando dado da mensagem
+        msg = msg['data']
+        if msg == REQUEST_GAME_INPUT:
+            # Caso mensagem seja sinalização de INPUT, enviar para Servidor seu Input (Ele está travado esperando)
+            msgToSend = createMessage(input("Enviar para server: "))
+            if msgToSend:
+                client_socket.send(msgToSend)
+        elif msg == SEND_STATUS:
+            # Caso seja uma sinalização de envio de dados, solicitar uma nova mensagem que já está sendo enviada pelo servidor
+            msg = receiveMessage(client_socket)
+            if msg:
+                status = msg['data']
+                # * Fazer o que quiser com o status
+                pass
+        elif msg == SEND_PLACAR:
+            # Caso seja uma sinalização de envio de dados, solicitar uma nova mensagem que já está sendo enviada pelo servidor
+            msg = receiveMessage(client_socket)
+            if msg:
+                placar = msg['data']
+                # * Fazer o que quiser com o placar
+                pass
--- Server/server_memory.py
+++ Server/server_memory.py
@@ -1,60 +1,152 @@
 import socket
 import time
 
-# Game Logic
+# * Server socket TCP
 
-nJogadores = 2
-vez = 0
-
-# Server socket TCP
-
+#! SETTAR IP DO HOST <<<<<
 HOST = "26.44.38.67"
 PORT = 50000
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
-server_socket.listen(0)
+server_socket.listen(2)
 
 client_list = []
 
-REQUEST_INPUT = "REQUEST_INPUT"
+# * Funções do Jogo
+
+
+def novoTabuleiro(dim):
+    # Cria um tabuleiro vazio.
+    tabuleiro = []
+    for i in range(0, dim):
+
+        linha = []
+        for j in range(0, dim):
+
+            linha.append(0)
+
+        tabuleiro.append(linha)
+
+    # Cria uma lista de todas as posicoes do tabuleiro. Util para
+    # sortearmos posicoes aleatoriamente para as pecas.
+    posicoesDisponiveis = []
+    for i in range(0, dim):
+
+        for j in range(0, dim):
+
+            posicoesDisponiveis.append((i, j))
+
+    # Varre todas as pecas que serao colocadas no
+    # tabuleiro e posiciona cada par de pecas iguais
+    # em posicoes aleatorias.
+        # * ALTERAÇÃO DO PORT: LIMITE SUPERIOR DO RANGE FORÇADAMENTE CONVERTIDO PRA INTEIRO
+    for j in range(0, int(dim / 2)):
+        for i in range(1, dim + 1):
+
+            # Sorteio da posicao da segunda peca com valor 'i'
+            maximo = len(posicoesDisponiveis)
+            indiceAleatorio = random.randint(0, maximo - 1)
+            rI, rJ = posicoesDisponiveis.pop(indiceAleatorio)
+
+            tabuleiro[rI][rJ] = -i
+
+            # Sorteio da posicao da segunda peca com valor 'i'
+            maximo = len(posicoesDisponiveis)
+            indiceAleatorio = random.randint(0, maximo - 1)
+            rI, rJ = posicoesDisponiveis.pop(indiceAleatorio)
+
+            tabuleiro[rI][rJ] = -i
+
+    return tabuleiro
+
+
+def novoPlacar(nJogadores):
+    return [0] * nJogadores
+
+
+# * Dados do Jogos
+
+dim = 4
+nJogadores = 2
+totalDePares = dim**2 / 2
+
+paresEncontrados = 0
+turno = 0
+
+tabuleiro = novoTabuleiro(dim)
+placar = novoPlacar(nJogadores)
+
+# * Message Settings
+
+HEADER_LENGTH = 10
+REQUEST_USERNAME = "#REQUEST_USERNAME_INPUT"
+REQUEST_GAME_INPUT = "#REQUEST_GAME_INPUT"
+SEND_STATUS = "#STATUS"
+SEND_TABULEIRO = "#TABULEIRO"
+SEND_PLACAR = "#PLACAR"
+SEND_VEZ = "#VEZ"
+
+
+def receiveMessage(socket):
+    try:
+        message_header = socket.recv(HEADER_LENGTH)
+
+        if not len(message_header):
+            return False
+
+        message_length = int(message_header.decode())
+        return {
+            "header": message_header,
+            "data": socket.recv(message_length).decode()
+        }
+    except:
+        return False
+
+
+def createMessage(message: str):
+    data = message.encode()
+    header = f'{len(data):<{HEADER_LENGTH}}'.encode()
+    return header + data
 
 
 def sendToAllClients(message):
-    byteMessage = str.encode(message)
-    print(f"Enviando para todo: {message}")
+    byteMessage = createMessage(message)
+    print(f"Enviando para todos: {message}")
     for client in client_list:
         client.send(byteMessage)
 
 
-def sendToClient(client: int, message: str):
+def sendToClient(client: socket, message: str):
     print(f"Enviando para {client}: {message}")
-    byteMessage = str.encode(message)
-    client_list[client].send(byteMessage)
+    byteMessage = createMessage(message)
+    client.send(byteMessage)
 
 
-def sendToExcept(ignoredClient: int, message: str):
+def sendToExcept(ignoredClient: socket, message: str):
     # Send to all clients exepct specified in parameter
     print(f"Enviando para todos exceto {ignoredClient}: {message}")
-    byteMessage = str.encode(message)
-    for index, client in enumerate(client_list):
-        if index != ignoredClient:
+    byteMessage = createMessage(message)
+    for client in client_list:
+        if client != ignoredClient:
             client.send(byteMessage)
 
 
-# Aceitando N clientes
+print(f"Servidor Iniciado, esperando {nJogadores} jogadores")
+
+# * Aceitando N clientes
 while True:
     client_socket, address = server_socket.accept()
 
     # ? Desnecessário porque limita conexão
     if len(client_list) >= nJogadores:
-        client_socket.send(str.encode("Servidor cheio!"))
+        client_socket.send(createMessage("Servidor cheio!"))
         client_socket.close()
         continue
 
     client_list.append(client_socket)
     print(f"Conexão com {address} estabelecida!")
-    client_socket.send(str.encode(
+    client_socket.send(createMessage(
         f"Conectado ao servidor!"))
 
     sendToAllClients(f"Jogadores: {len(client_list)}/{nJogadores}")
@@ -66,22 +158,19 @@ while True:
         break
 
 while True:
-    # time.sleep(2)
-    # sendToAllClients("Teste!")
-    # print("Teste enviado!")
+    sendToAllClients(f"Vez do jogador {turno+1}.\n")
+    sendToExcept(turno, f"Esperando jogador...")
 
-    # * sendToAllClients(Tabuleiro)
-    sendToAllClients(f"Vez do jogador {vez+1}.\n")
-    sendToExcept(vez, f"Esperando jogador...\n")
+    print(f"Solicitando input do jogador {turno+1}")
 
-    print(f"Solicitando input do jogador {vez+1}")
+    sendToClient(turno, f"\nEscolha uma peça:\n>")
+    sendToClient(turno, REQUEST_GAME_INPUT)
 
-    sendToClient(vez, f"Escolha uma peça:\n")
-    sendToClient(vez, REQUEST_INPUT)
+    playerInput = receiveMessage(client_list[turno])
+    if playerInput:
+        msg = playerInput['data']
+        print(f"Mensagem recebida do jogador {turno+1}: {msg}")
 
-    msg = client_list[vez].recv(1024).decode()
-    print(f"Mensagem recebida do jogador {vez+1}: {msg}")
+    time.sleep(0.5)
 
-    time.sleep(2)
-
-    vez = (vez + 1) % nJogadores
+    turno = (turno + 1) % nJogadores
